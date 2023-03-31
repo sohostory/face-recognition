26875cc8e8bc11f744e8c2e3dedb1940f29e3b1f
sohostory <sohostory@gmail.com>
2023-03-30 20:24:53 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import ParticlesBg from "particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#fff" type="cobweb" num={250} bg={true} />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
+      <Rank />
+      <ImageLinkForm />
+      {/*
     <FaceRecognition />} */}
     </div>
   );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
+import brain from "./brain.png";
 import "./Logo.css";
 
 const Logo = () => {
   return (
-    <div className="ma4 m40" style={{ width: "150px" }}>
+    <div className="ma4 mt0" style={{ width: "150px" }}>
       <Tilt className="Tilt br2 shadow-2">
         <div
-          className="Tilt-inner"
+          className="Tilt-inner pa3"
           style={{
             height: "150px",
-            backgroundColor: "darkgreen",
           }}
         >
-          👽
+          <img style={{ paddingTop: "5px" }} alt="logo" src={brain} />
         </div>
       </Tilt>
     </div>
