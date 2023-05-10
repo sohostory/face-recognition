9d2f23a8ae656aaedb45cac43fb7dfc2b701c904
sohostory <sohostory@gmail.com>
2023-05-09 22:04:35 -0500

    production ready

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     console.log("click");
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://afternoon-everglades-19787.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -81,7 +81,7 @@ class App extends Component {
       .then((response) => response.json()) //.json()
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://afternoon-everglades-19787.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
